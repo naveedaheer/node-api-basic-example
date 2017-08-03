e6ef8b92a29b06be0e420451a5f3f4c075bce258
naveedaheer <naveedaheer@gmail.com>
2017-08-03 16:05:04 +0500

    Code moved from demo.js to index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:8080/')
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.port || 8080;
+
+var router = express.Router();
+
+router.get('/', function (req, res) {
+    res.json({ message: 'Hello World! this is testing message' });
+});
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Server is running on port : ' + port);
